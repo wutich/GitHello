96c0b3a90cb66588e5747e1dec8c9ae12a192f6b
zx <vsc@vsctekiimac-2.local>
2016-04-20 16:17:27 +0800

    合并

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -18,8 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Hello world Git");
-    
-    [self sayHelloWorld];
 }
 
 - (void)didReceiveMemoryWarning {
