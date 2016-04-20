ad9df56ee0caaf82ca8c5c685853ddd3f86e89ea
zx <vsc@vsctekiimac-2.local>
2016-04-20 15:30:04 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by zx on 16/4/20.
+//  Copyright © 2016年 com.vic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by zx on 16/4/20.
+//  Copyright © 2016年 com.vic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
